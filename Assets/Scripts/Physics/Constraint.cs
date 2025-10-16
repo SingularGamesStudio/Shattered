e747cbbe74026ef1664e874e2e2e7f15faa8617a
Kiryanov Grigory <40793299+SingularGamesStudio@users.noreply.github.com>
2025-10-16 21:54:22 +0300

    refactor: add generic constraint structure

---
+++ Assets/Scripts/Physics/Constraint.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+namespace Physics {
+    public interface Constraint {
+        // Initialise stores needed cached data before making an iteration step.
+        // For example, it can calculate the non-deformed neighbor distances before the position shifts.
+        void Initialise(List<Node> nodes) { }
+        // Relax evaluates the constraint, and relaxes the positions to satisfy it.
+        // It returns the updated total Lagrange Multiplier.
+        float Relax(List<Node> nodes, float stiffness, float lagrangeMult) {
+            return lagrangeMult;
+        }
+    }
+
+    public class ConstraintCache {
+        public List<int> neighbors;
+        public List<float> neighborDistances;
+    }
+}
